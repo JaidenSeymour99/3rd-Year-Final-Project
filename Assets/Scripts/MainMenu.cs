9b210ec218caa058f041ed857afaeaf5de730ed9
Joshua Seymour <j0shuadan1elseym0ur@gmail.com>
2021-01-28 16:22:27 +0000

    Creating Menu's
    Creating main menu and options menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Main menu class, play game, quit game.
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    //Quits the game.
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
